1cd4751be3e75c76b7ca3a35e8d75efb1b06adcd
Henry <cryptix@riseup.net>
2016-06-11 22:40:19 +0200

    go1.7 adaptation

--- handler_test.go
+++ handler_test.go
@@ -1,35 +1,37 @@
 package xstats
 
 import (
+	"context"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/rs/xhandler"
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/net/context"
 )
 
 func TestHandler(t *testing.T) {
 	s := &fakeSender{}
-	n := xhandler.HandlerFuncC(func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		xs, ok := FromContext(ctx).(*xstats)
+	n := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		xs, ok := FromContext(r.Context()).(*xstats)
 		assert.True(t, ok)
 		assert.Equal(t, s, xs.s)
 		assert.Equal(t, []string{"envtag"}, xs.tags)
 	})
 	h := NewHandler(s, []string{"envtag"})(n)
-	h.ServeHTTPC(context.Background(), nil, nil)
+	req := httptest.NewRequest("", "/", nil)
+	h.ServeHTTP(nil, req.WithContext(context.Background()))
 }
 
 func TestHandlerPrefix(t *testing.T) {
 	s := &fakeSender{}
-	n := xhandler.HandlerFuncC(func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		xs, ok := FromContext(ctx).(*xstats)
+	n := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		xs, ok := FromContext(r.Context()).(*xstats)
 		assert.True(t, ok)
 		assert.Equal(t, s, xs.s)
 		assert.Equal(t, []string{"envtag"}, xs.tags)
 		assert.Equal(t, "prefix.", xs.prefix)
 	})
 	h := NewHandlerPrefix(s, []string{"envtag"}, "prefix.")(n)
-	h.ServeHTTPC(context.Background(), nil, nil)
+	req := httptest.NewRequest("", "/", nil)
+	h.ServeHTTP(nil, req.WithContext(context.Background()))
 }
