99257486f61226ce8a4ecc420234f3dff1641d4b
Glen D Sanford <9len@users.noreply.github.com>
2017-03-17 20:37:23 -0700

    add support for scoping (#18)

--- xstats_example_test.go
+++ xstats_example_test.go
@@ -29,3 +29,28 @@ func ExampleNew() {
 	s.Count("requests", 1, "tag")
 	s.Timing("something", 5*time.Millisecond, "tag")
 }
+
+func ExampleNewScoping() {
+	// Defines interval between flushes to statsd server
+	flushInterval := 5 * time.Second
+
+	// Connection to the statsd server
+	statsdWriter, err := net.Dial("udp", "127.0.0.1:8126")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the stats client
+	s := xstats.NewScoping(dogstatsd.New(statsdWriter, flushInterval), ".", "my-thing")
+
+	// Global tags sent with all metrics (only with supported clients like datadog's)
+	s.AddTags("role:my-service", "dc:sv6")
+
+	// Send some observations
+	s.Count("requests", 1, "tag")
+	s.Timing("something", 5*time.Millisecond, "tag")
+
+	// Scope the client
+	ss := xstats.Scope(s, "my-sub-thing")
+	ss.Histogram("latency", 50, "tag")
+}
