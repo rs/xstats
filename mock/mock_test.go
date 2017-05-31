853db44e10bd648987ecad3d7e212fe45365f5fe
Dmitry Lagoza <dmitry@lagoza.name>
2017-05-31 20:09:10 +0300

    Mock sender for xstats mocking on testing stage (#21)

---
+++ mock/mock_test.go
@@ -0,0 +1,28 @@
+package mock
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/rs/xstats"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	s := New()
+	assert.Implements(t, (*xstats.Sender)(nil), s)
+}
+
+func TestSender(t *testing.T) {
+	s := New()
+	d := time.Second
+	tags := []string{"2", "3"}
+	s.On("Timing", "1", d, tags)
+	s.Timing("1", d, tags...)
+	s.On("Count", "1", 2.0, tags)
+	s.Count("1", 2.0, tags...)
+	s.On("Gauge", "1", 2.0, tags)
+	s.Gauge("1", 2.0, tags...)
+	s.On("Histogram", "1", 2.0, tags)
+	s.Histogram("1", 2.0, tags...)
+	s.AssertExpectations(t)
+}
